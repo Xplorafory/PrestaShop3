7b3ced6f5915110a435ae47604d8b613345dc17c
Jonas Breisel <jonas.breisel@xploraforytest.se>
2020-11-23 11:15:17 +0100

    added Readme file

---
+++ Readme.java
@@ -0,0 +1,9 @@
+Feature: Fill in form feature to fill in a form on a webpage
+
+  @testing
+  Scenario: Login as a authenticated user
+    Given user is on form homepage
+    When user fill in name "kalle" and job title
+    And user enters level of education years of experience
+    And press submit button
+    Then success message is displayed
