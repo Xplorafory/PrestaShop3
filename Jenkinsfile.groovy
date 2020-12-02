58827e669ef4fded14c7e336865b7f2797c04ec4
Jonas Breisel <jonas.breisel@xploraforytest.se>
2020-12-02 22:17:47 +0100

    changed Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,17 @@
 pipeline {
     agent any 
+
     stages {
         stage("build") {
             steps {
                 echo 'building the application...'
             }
         }
+
         stage("test") {
         	echo 'testing the application...'
         }
+        
         stage("deploy") {
         	steps{
         		echo 'deploying the application...'
